82fa5f1de54ee3a8e7516b94bba74e563a8b3a40
KANEEZ SULTANA <131600813+KANEEZ023@users.noreply.github.com>
2023-08-04 21:17:23 +0530

    Add files via upload

---
+++ LANDING PAGE/faq.js
@@ -0,0 +1,11 @@
+//--------FAQ page -------//
+
+
+const accordion = document.getElementsByClassName('contentBx');
+
+            for (i = 0; i<accordion.length; i++){
+                accordion[i].addEventListener('click', function(){
+                    this.classList.toggle('active')
+                })
+            }
+//--------End FAQ page -------//
